59141423b1dd5366d12cf315ce28aec5c45bd5be
Tobias de Castilho <dev@tobiasdecastilho.com.br>
2024-09-19 20:24:42 -0300

    add: out defs

---
+++ src/out.cpp
@@ -0,0 +1,8 @@
+_Pragma("once")
+
+#include <iostream>
+
+#define _SUCESS		std::cout << "\033[1;32m[SUCCESS] \033[0m"
+#define _ERROR		std::cout << "\033[1;31m[ERROR] \033[0m"
+#define _WARNING	std::cout << "\033[1;33m[WARNING] \033[0m"
+#define _INFO			std::cout << "\033[1;34m[INFO] \033[0m"
